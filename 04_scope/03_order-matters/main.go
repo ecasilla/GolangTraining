2e066ff19b0ad7fa7043e01bb2c4e87a7061d143
Ernie Casilla <ecasilla@icloud.com>
2016-02-18 13:13:12 -0500

    finished blank identifier

--- 04_scope/03_order-matters/main.go
+++ 04_scope/03_order-matters/main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	x := 1
 }
+
+var y int = 0
--- 04_scope/04_variable-shadowing/main.go
+++ 04_scope/04_variable-shadowing/main.go
@@ -1,4 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
+
+func max(x int) int {
+	return 42 + x
 }
+
+func main() {
+	max := max(8)
+	fmt.Println(max) // max is now the result, not the function
+}
+// don't do this; bad coding practice to shadow variables
